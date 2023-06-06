62c7c27cea3098d1e779f2a131e3599c8c98522a
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-06 07:43:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import data from "./data"
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  useState(data);
+
+  return(
+    <>
+    <h2>Birthday reminder</h2>
+    </>
+  )
 };
 export default App;
